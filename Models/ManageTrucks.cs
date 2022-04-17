b953bd7685d4fff7615d0d59052b50928d4a21de
Ryan Bills <rbills@atu.edu>
2022-04-16 22:49:38 -0500

    Multiple changes. Added pages for Drivers, Trucks/Trailers and linked to NavMenu Created Model library. Made a few cosmetics changes, including adding new logo to NavMenu.

---
+++ Models/ManageTrucks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD.Models
+{
+    public partial class ManageTrucks
+    {
+        public int TruckNumber { get; set; }
+        public string TruckMake { get; set; }
+        public string TruckModel { get; set; }
+    }
+}
